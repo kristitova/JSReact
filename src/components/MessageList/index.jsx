c239ec6e2e0c5f971297167f315e9b45702b86f2
kristina <kristina-danilova1994@mail.ru>
2021-10-25 23:27:38 +0300

    Файлы для задания из 2 урока

---
+++ src/components/MessageList/index.jsx
@@ -0,0 +1,11 @@
+import React, { useCallback } from 'react';
+import * as styles from './MessageList.module.css';
+
+
+export const MessageList = ({ messages }) => {
+    const renderMessage = useCallback((mess) => (
+        <div className={styles.messList}> {mess.author} : {mess.text}</div >
+    ), []);
+
+    return messages.map(renderMessage)
+};
