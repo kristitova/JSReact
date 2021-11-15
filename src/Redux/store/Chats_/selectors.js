9de2b9f021f7d8a66c0a1cc44ab98f424ebfbe67
kristitova <66868727+kristitova@users.noreply.github.com>
2021-11-16 00:51:40 +0300

    Update selectors.js

--- src/Redux/store/Chats_/selectors.js
+++ src/Redux/store/Chats_/selectors.js
@@ -5,23 +5,6 @@ export const getChats = (state) => getRootChats(state).chatList;
 export const hasChatbyId = (chatId) => (state) => {
   return getChats(state).find(({ idchat }) => idchat == chatId) || {}
 }
-  //export const hasChatbyId = (chatId) => (state) => getChats(state).findIndex((item) => item.idchat == chatId) !== -1
 
-//export const hasChatbyId = (chatId) => (state) => {
-// console.log(getChats(state).findIndex((item) => item.idchat === chatId) !== -1)
-//  return getChats(state).findIndex((item) => item.idchat === chatId) !== -1
-//};
-
-//export const hasChatbyId = (chatId) => (state) => getChats(state).findIndex(compare(chatId)) !== -1;
-//export const hasChatbyId = (chatId) => (state) => {
-//  console.log(getChats(state).find(({ idchat }) => idchat == chatId));
-//   return
-//}
-//export const hasChatbyId = (chatId) => (state) => getChats(state).findIndex((item) => item.idchat == chatId) !== - 1 || {}
-
-
-   // console.log(getChats(state).findIndex((item) => item.idchat == chatId));
-  //  return getChats(state).findIndex((item) => item.idchat == chatId) !== - 1 || {};
-//}
 
 
